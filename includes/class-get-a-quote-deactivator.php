772ce2fbcf349c54d9256e5e370a95d1081e84ca
cedashu <ashutoshverma@makewebbetter.com>
2020-10-05 13:20:16 +0530

    Boilerplate

---
+++ includes/class-get-a-quote-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://makewebbetter.com
+ * @since      1.0.0
+ *
+ * @package    Get_A_Quote
+ * @subpackage Get_A_Quote/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Get_A_Quote
+ * @subpackage Get_A_Quote/includes
+ * @author     Make Web Better <plugins@makewebbetter.com>
+ */
+class Get_A_Quote_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ includes/class-get-a-quote-i18n.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       https://makewebbetter.com
+ * @since      1.0.0
+ *
+ * @package    Get_A_Quote
+ * @subpackage Get_A_Quote/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    Get_A_Quote
+ * @subpackage Get_A_Quote/includes
+ * @author     Make Web Better <plugins@makewebbetter.com>
+ */
+class Get_A_Quote_i18n {
+
+
+	/**
+	 * Load the plugin text domain for translation.
+	 *
+	 * @since    1.0.0
+	 */
+	public function load_plugin_textdomain() {
+
+		load_plugin_textdomain(
+			'get-a-quote',
+			false,
+			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
+		);
+
+	}
+
+
+
+}
