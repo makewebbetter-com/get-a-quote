fe101616ddcda7ac372889206703ff6aefbb06e5
shailesh-cedcoss <shaileshkr.dubey@cedcoss.com>
2021-01-29 20:04:40 +0530

    delete action in live page builder completed

--- admin/js/get-a-quote-admin.js
+++ admin/js/get-a-quote-admin.js
@@ -3,8 +3,7 @@ jQuery(document).ready(function($) {
     /**
      * Constants.
      */
-    // const deleteButton = '<i id="icon_del" class="fas fa-trash-alt"></i>';
-    const label = '<lable for="{{fields-id-here}}" class="mwb-gaq-fields-label"><b>{{fields-label-here}}</b><i id="icon_del" class="fas fa-trash-alt"></i></label>';
+    const label = '<lable for="{{fields-id-here}}" class="mwb-gaq-fields-label">{{fields-label-here}}<i id="{{fields-scope}}" class="fas fa-trash-alt icon_del"></i></label>';
     const wrapper = '<div id="form-group-{{fields-scope-here}}" class="form-group"></div>';
 
     /**
@@ -18,7 +17,7 @@ jQuery(document).ready(function($) {
         jQuery('.mwb_gaq__modal-wrapper').css('overflow', 'hidden');
     });
 
-
+    
     // drawer query start
     jQuery('.mwb_gaq_open__drawer').on('click', function() {
         jQuery(this).parents('.mwb_gaq_container').addClass('active');
@@ -28,6 +27,30 @@ jQuery(document).ready(function($) {
         jQuery(this).parents('.mwb_gaq_container').removeClass('active');
     });
 
+    // deletion of fields
+    // jQuery(".icon_del").on('click', function() {
+    //     var attrs = jQuery( this );
+    //     alert(attrs[0].id);
+    // });
+
+      
+    jQuery(document).on("click", ".icon_del", function() {
+        var attrs = jQuery( this );
+        var classname = '#form-group-';
+        var result = classname.concat( attrs[0].id );
+        result = jQuery( result ).hide();
+        // console.log(result);
+        // result.remove()
+        //result = $( result );
+        //jQuery()
+        //alert( result );
+        // alert(attrs[0].id);
+        // var attrs = jQuery( this );
+        // var id = $( attrs[0].id );
+        // console.log( id );
+        // jQuery( id ).remove();
+    });
+    
     // Insertion of new fields in form.
     jQuery('.mwb_gaq_commonFields_group').on('click', function() {
 
@@ -55,12 +78,11 @@ jQuery(document).ready(function($) {
                 case 'name':
                     newElement.setAttribute( key, value );
                     break;
-
                 case 'label':
                     newLabel = label.replace( "{{fields-id-here}}", attrs.id );
                     newLabel = newLabel.replace( "{{fields-label-here}}", attrs.label );
+                    newLabel = newLabel.replace( "{{fields-scope}}", attrs.scope );
                     break;
-
                 default:
                     break;
             }    
@@ -68,10 +90,10 @@ jQuery(document).ready(function($) {
 
         newWrapper = wrapper.replace( "{{fields-scope-here}}", attrs.scope );        
         var labelHTML = $(newLabel);
-        var divHTML = $( newWrapper );
-        divHTML.append( labelHTML );
-        divHTML.append( newElement );
-        jQuery( ".form-group:last" ).append( divHTML );
+        var divHTML = $(newWrapper);
+        divHTML.append(labelHTML);
+        divHTML.append(newElement);
+        jQuery(".form-group:last ").append(divHTML);
     }
 
     // Add class to body on builder open.
@@ -90,9 +112,6 @@ jQuery(document).ready(function($) {
         if (!results[2]) return '';
         return decodeURIComponent(results[2].replace(/\+/g, ' '));
     }
-    // $( '.mwb_form_first_name' ).on('click', function() {
-    //     $(".form-group:last").append( '<div id="form-group-first-name" class="form-group"><label for="ffname"><b>First Name*</b><i id="icon_del" class="fas fa-trash-alt"></i></label><input type="text" required="required" class="form-control" id="ffname" pattern="[a-zA-Z0-9 ]+" required="required" value="" size="40" placeholder="First Name" /></div>' );
-    // })
 
 // End of scripts.	
 });
