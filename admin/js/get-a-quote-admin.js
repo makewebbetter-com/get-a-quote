d248674a38d9e368326b234a6e06845698462dcb
shailesh-cedcoss <shaileshkr.dubey@cedcoss.com>
2021-02-01 20:06:17 +0530

    changing values using jQuery on time of edit in live builer

--- admin/js/get-a-quote-admin.js
+++ admin/js/get-a-quote-admin.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($) {
     /**
      * Constants.
      */
-    const label = '<lable for="{{fields-id-here}}" class="mwb-gaq-fields-label">{{fields-label-here}}<i id="{{fields-scope}}" class="fas fa-trash-alt icon_del"></i></label>';
+    const label = '<lable for="{{fields-id-here}}" class="{{field}}">{{fields-label-here}}</label><i id="{{fields-scope}}" class="fas fa-trash-alt icon_del"></i><i data-id="{{scope}}" class="far fa-edit icon_edit"></i>';
     const wrapper = '<div id="form-group-{{fields-scope-here}}" class="form-group"></div>';
 
     /**
@@ -20,43 +20,89 @@ jQuery(document).ready(function($) {
     
     // drawer query start
     jQuery('.mwb_gaq_open__drawer').on('click', function() {
-        jQuery(this).parents('.mwb_gaq_container').addClass('active');
+        removecls( jQuery(this), 'inactive');
+        addcls( jQuery(this), 'active');
     });
 
+    //For closing the adding field div.
     jQuery('#mqb_gaq_close__drawer').on('click', function() {
-        jQuery(this).parents('.mwb_gaq_container').removeClass('active');
+        removecls( jQuery(this), 'active');
     });
+    
+    //change the Label name on edit.
+    jQuery(document).on("change paste keyup", "#field_name", function() {
+        var attrt = jQuery( this ).data();
+        var val = "#";
+        attrt = val.concat( attrt.key );
+        jQuery( attrt ).text( $(this).val() );
+	}); 
 
-    // deletion of fields
-    // jQuery(".icon_del").on('click', function() {
-    //     var attrs = jQuery( this );
-    //     alert(attrs[0].id);
-    // });
+    //change the Label name on edit.
+    jQuery(document).on("change paste keyup", "#field_place_name", function() {
+        var attrt = jQuery( this ).data();
+        var val = "#";
+        attrt = val.concat( attrt.key );
+        // console.log(  );
+        jQuery(attrt).attr("placeholder", $(this).val());
+	}); 
 
-      
+    //for deleting the div using Icon.
     jQuery(document).on("click", ".icon_del", function() {
         var attrs = jQuery( this );
         var classname = '#form-group-';
         var result = classname.concat( attrs[0].id );
-        result = jQuery( result ).hide();
-        // console.log(result);
-        // result.remove()
-        //result = $( result );
-        //jQuery()
-        //alert( result );
-        // alert(attrs[0].id);
-        // var attrs = jQuery( this );
-        // var id = $( attrs[0].id );
-        // console.log( id );
-        // jQuery( id ).remove();
+        removecls( jQuery(this), 'inactive');
+        result = jQuery( result ).remove();
+        // alert( result );
     });
-    
+
+    //for editing the value using the edit logo.
+    jQuery('#mwb_gaq_close_form_editer').on('click', function() {
+        removecls( jQuery(this), 'inactive');
+    });
+
+    jQuery(document).on("click", ".icon_edit", function() {
+        removecls( jQuery(this), 'active');
+        addcls( jQuery(this), 'inactive' );
+        var attrs = jQuery( this ).data();
+        var classname = '#form-group-';
+        var result = classname.concat( attrs.id );
+        child = jQuery( result ).children();
+        $( child ).each(function( index ) {
+            if( ( $( this ).text() ) != "" ) {
+                $("#field_name").attr("value", $( this ).text());
+                // console.log($( this )[0].id );
+                $("#field_name").attr("data-key", $( this )[0].id);
+            }
+            if ( ( $( this )[0].placeholder ) !== undefined ) {
+                // console.log( $( this )[0] );
+                $("#field_place_name").attr("data-key", $( this )[0].id);
+                $("#field_place_name").attr("value", $( this )[0].placeholder);
+            }
+        });
+    });
+
+    //remove the class.
+    function removecls( obj, cname ) {
+        obj.parents('.mwb_gaq_container').removeClass( cname );
+    }
+
+    // add the class.
+    function addcls( obj, cname ) {
+        obj.parents('.mwb_gaq_container').addClass( cname );
+    }
+
     // Insertion of new fields in form.
     jQuery('.mwb_gaq_commonFields_group').on('click', function() {
 
         var attrs = jQuery( this ).data();
         appendNewlement( attrs );
     });
+
+    // Submit the form fields for front-end display
+    // jQuery('.mwb_gaq__form__submit').on('click', function() {
+    //     console.log( $('#append-form').children() );
+    // });
 
     /**
      * Library Functions here.
@@ -80,20 +126,21 @@ jQuery(document).ready(function($) {
                     break;
                 case 'label':
                     newLabel = label.replace( "{{fields-id-here}}", attrs.id );
+                    newLabel = newLabel.replace( "{{field}}", attrs.lname );
                     newLabel = newLabel.replace( "{{fields-label-here}}", attrs.label );
                     newLabel = newLabel.replace( "{{fields-scope}}", attrs.scope );
+                    newLabel = newLabel.replace( "{{scope}}", attrs.scope );
                     break;
                 default:
                     break;
             }    
         });
-
-        newWrapper = wrapper.replace( "{{fields-scope-here}}", attrs.scope );        
+        newWrapper = wrapper.replace( "{{fields-scope-here}}", attrs.scope );  
         var labelHTML = $(newLabel);
         var divHTML = $(newWrapper);
         divHTML.append(labelHTML);
         divHTML.append(newElement);
-        jQuery(".form-group:last ").append(divHTML);
+        jQuery("#append-form ").append(divHTML);
     }
 
     // Add class to body on builder open.
@@ -115,4 +162,3 @@ jQuery(document).ready(function($) {
 
 // End of scripts.	
 });
-
--- admin/partials/templates/mwb-gaq-edit-form-fields.php
+++ admin/partials/templates/mwb-gaq-edit-form-fields.php
@@ -33,65 +33,69 @@
 	<div class="mwb_gaq_container">
 		<div id="mwb_gaq__form" class="mwb_gaq__form">
 			<form method="post" class="form-group-fields">
-				<div id="form-group-sname" class="form-group">
-					<label for="name">Name*<i id="sname" class="fas fa-trash-alt icon_del"></i></label>
-					<input id="name" type="text" required="required" placeholder="Enter your name" class="form-control">
+				<div id="append-form">
+					<div id="form-group-sname" class="form-group">
+						<label for="name" id="nlname">Name*</label><i id="sname" class="fas fa-trash-alt icon_del"></i><i data-id="sname" class="far fa-edit icon_edit"></i>
+						<input id="name" type="text" required="required" placeholder="Enter your name" class="form-control">
+					</div>
 				</div>
-				<div id="form-group-semail" class="form-group">
-					<label for="email">Email*<i id="semail"class="fas fa-trash-alt icon_del"></i></label>
-					<input id="email" type="email" name="email" required="required" placeholder="Enter Your email" class="form-control">
-				</div>
-				<div id="form-group-smessage" class="form-group">
-					<label for="message">Message<i id="smessage" class="fas fa-trash-alt icon_del"></i></label>
-					<textarea id="message" name="message" rows="3" class="form-control"></textarea>
-				</div>
-
-				<button class="mwb_gaq__form__submit btn btn-info">Submit</button>
+				<a href="#" class="mwb_gaq__form__submit btn btn-info">Submit</a>
 			</form>
 		</div>
 		<div id="mwb_gaq_commonFields" class="mwb_gaq__commonFields">
 			<div class="mwb_gaq_close__drawer">
 				<button id="mqb_gaq_close__drawer">Done</button>
 			</div>
-			<div class="mwb_gaq_commonFields_group mwb_form_first_name" data-ftype="input" data-type="text" data-name="ffname" data-id="fname" data-placeholder="First Name" data-size="40" data-class="form-control" data-required="required" data-scope="sfirstname" data-label="First Name" >
+			<div class="mwb_gaq_commonFields_group mwb_form_first_name" data-lname="flname" data-ftype="input" data-type="text" data-name="ffname" data-id="fname" data-placeholder="First Name" data-size="40" data-class="form-control" data-required="required" data-scope="sfirstname" data-label="First Name" >
 				<div id="mwb_gaq_commonFields_icon" ><span id='icon_span' class="fas fa-file-signature"></span></div>
 				First name
 			</div>
-			<div class="mwb_gaq_commonFields_group mwb_form_city" data-ftype="input" data-type="text" data-name="fcityname" data-id="city" data-placeholder="City name here" data-size="40" data-class="form-control" data-scope="scityname" data-label="City">
+			<div class="mwb_gaq_commonFields_group mwb_form_city" data-ftype="input" data-lname="clname" data-type="text" data-name="fcityname" data-id="city" data-placeholder="City name here" data-size="40" data-class="form-control" data-scope="scityname" data-label="City">
 				<div id="mwb_gaq_commonFields_icon" ><span id='icon_span' class="fas fa-search-location"></span></div>
 				City
 			</div>
-			<div class="mwb_gaq_commonFields_group mwb_form_email" data-ftype="input" data-type="email" data-name="email" data-id="email" data-placeholder="Email here" data-size="40" data-class="form-control" data-scope="ssemail" data-label="Email">
+			<div class="mwb_gaq_commonFields_group mwb_form_email" data-ftype="input" data-lname="elname" data-type="email" data-name="email" data-required="required" data-id="email" data-placeholder="Email here" data-size="40" data-class="form-control" data-scope="ssemail" data-label="Email">
 				<div id="mwb_gaq_commonFields_icon" ><span id='icon_span' class="far fa-envelope-open"></span></div>
 				Email			
 			</div>
-			
-			<div class="mwb_gaq_commonFields_group mwb_form_file" data-ftype="input" data-type="file" data-name="ffiles" data-id="files" data-class="form-control" data-scope="sfile" data-label="File">
+			<div class="mwb_gaq_commonFields_group mwb_form_file" data-ftype="input" data-lname="flfile"  data-type="file" data-name="ffiles" data-id="files" data-class="form-control" data-scope="sfile" data-label="File">
 				<div id="mwb_gaq_commonFields_icon" ><span id='icon_span' class="fas fa-file-alt"></span></div>
 				File		
 			</div>
-			<div class="mwb_gaq_commonFields_group mwb_form_country" data-ftype="input" data-type="text" data-name="fcountry" data-placeholder="Enter Country Name" data-id="fcountry" data-class="form-control" data-scope="scountry" data-label="Country">
+			<div class="mwb_gaq_commonFields_group mwb_form_country" data-ftype="input" data-lname="colname"  data-type="text" data-name="fcountry" data-placeholder="Enter Country Name" data-id="fcountry" data-class="form-control" data-scope="scountry" data-label="Country">
 				<div id="mwb_gaq_commonFields_icon" ><span id='icon_span' class="far fa-flag"></span></div>
 				Country		
 			</div>
-			<div class="mwb_gaq_commonFields_group mwb_form_zipcode" data-inputmode="numeric" data-pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$" data-placeholder="Zipcode here" data-ftype="input" data-type="text" data-name="zipcode" data-id="zipcodes" data-class="form-control" data-scope="szipcode" data-label="Zipcode">
+			<div class="mwb_gaq_commonFields_group mwb_form_zipcode" data-inputmode="numeric"  data-lname="zlname" data-pattern="^(?(^00000(|-0000))|(\d{5}(|-\d{4})))$" data-placeholder="Zipcode here" data-ftype="input" data-type="text" data-name="zipcode" data-id="zipcodes" data-class="form-control" data-scope="szipcode" data-label="Zipcode">
 				<div id="mwb_gaq_commonFields_icon" ><span id='icon_span' class="far fa-address-card"></span></div>
 				Zipcode		
 			</div>
-			<div class="mwb_gaq_commonFields_group mwb_form_states" data-ftype="input" data-type="text" data-name="fstates" data-placeholder="Enter States Name" data-id="fstates" data-class="form-control" data-scope="sstates" data-label="States">
+			<div class="mwb_gaq_commonFields_group mwb_form_states" data-ftype="input"  data-lname="slname"  data-type="text" data-name="fstates" data-placeholder="Enter States Name" data-id="fstates" data-class="form-control" data-scope="sstates" data-label="States">
 				<div id="mwb_gaq_commonFields_icon" ><span id='icon_span' class="fas fa-flag-usa"></span></div>
 				States		
 			</div>
-			<div class="mwb_gaq_commonFields_group mwb_form_budget" data-ftype="input" data-type="number" data-name="fbudget" data-placeholder="Budget" data-id="fbudget" data-class="form-control" data-scope="sbudget" data-label="Budget">
+			<div class="mwb_gaq_commonFields_group mwb_form_budget" data-ftype="input" data-lname="blname"  data-type="number" data-name="fbudget" data-placeholder="Budget" data-id="fbudget" data-class="form-control" data-scope="sbudget" data-label="Budget">
 				<div id="mwb_gaq_commonFields_icon" ><span id='icon_span' class="fas fa-dollar-sign"></span></div>
 				Budget	
-			</div><div class="mwb_gaq_commonFields_group mwb_form_phone" data-ftype="input" data-type="tel" data-name="phone" data-placeholder="123-45-67-890" data-pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{3}" data-id="phone" data-class="form-control" data-scope="sphone" data-label="Phone Number">
+			</div>
+			<div class="mwb_gaq_commonFields_group mwb_form_phone" data-ftype="input" data-lname="plname"  data-type="tel" data-required="required" data-name="phone" data-placeholder="123-45-67-890" data-pattern="[0-9]{3}-[0-9]{2}-[0-9]{2}-[0-9]{3}" data-id="phone" data-class="form-control" data-scope="sphone" data-label="Phone Number">
 				<div id="mwb_gaq_commonFields_icon" ><span id='icon_span' class="fas fa-mobile-alt"></span></div>
 				Phone	
 			</div>
 		</div>
 		<div class="mwb_gaq_open__drawerwrapper">
 			<a href="#" class="mwb_gaq_open__drawer"><i class="fa fa-plus fa-3x"></i></a>
+		</div>
+		<div class="mwb_gaq_edit_container">
+			<div class='mwb_gaq_edit_form_fields' class="mwb_gaq_edit_fields">
+				<button id="mwb_gaq_close_form_editer">Done</button>
+				<br>
+				<hr>
+				<label for="field_name">Name</label><br>
+				<input type="text" data-key="" id="field_name" value="" class="field" placeholder="name"><br>
+				<label for="field_place_name">Placeholder-Name</label><br>
+				<input type="text" data-key="" id="field_place_name" valu="" class="field" placeholder="place">
+			</div>
 		</div>
 	</div>
 </div>
